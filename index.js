6cebe4f93f55b84a89d8b9d6222cc7673812d267
Arup Debnath <1805043@ugrad.cse.buet.ac.bd>
2023-06-13 21:57:05 +0600

    An API to get user specific selected classes

--- index.js
+++ index.js
@@ -186,7 +186,20 @@ async function run() {
       res.send(result);
     });
 
-
+    app.get('/selected-classes', verifyJWT, verifyStudent, async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res.status(403).send({ error: true, message: 'forbidden access' })
+      }
+      const query = { email: email };
+      const cursor = selectedClassesCollection.find(query);
+      const classes = await cursor.toArray();
+      res.send(classes);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
